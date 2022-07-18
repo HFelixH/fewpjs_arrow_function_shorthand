651f9b165b258bce59539b50f25dcbe2fa1ec7c2
HFelixH <felix-hoang@outlook.com>
2022-07-18 18:24:23 +0930

    Upload Arrow Functions Lab

---
+++ index.js
@@ -0,0 +1,20 @@
+let divide = () => 2000/100
+
+console.log(divide())
+
+//================
+
+let square = x => x * x;
+
+let x = 2
+
+console.log(square(x))
+
+//================
+
+let add = (a, b) => a + b;
+
+let a = 3
+let b = 4
+
+console.log(add(a,b))
